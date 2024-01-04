3f8e6aab7bf84919eee8b48c2017366d58dc096f
Miguel <miguelgermanico@gmail.com>
2024-01-04 04:16:30 +0100

    arreglos

--- ProyectoAjedrezMiguel/src/proyectoajedrezmiguel/Dama.java
+++ ProyectoAjedrezMiguel/src/proyectoajedrezmiguel/Dama.java
@@ -15,21 +15,26 @@ public class Dama extends Pieza{
         super(esBlanco, valor, nombre);
     }
     public boolean comprobacionMov(int f1, int c1, int f2, int c2){
-        boolean bien = true;
+        boolean bien;
+        int dis = 0;
         int movimientoDer = 0, movimientoIzq = 0, movimientoArr = 0, movimientoAba = 0;
         bien = (AjedUtils.distancia(f1, f2) == AjedUtils.distancia(c1, c2)) || c1 == c2 || f1 == f2;
         if (bien) {
             if (f2 < f1) {                                                                               //subiendo
                 movimientoArr = -1;
+                dis = AjedUtils.distancia(f1, f2);
             }else if(f1 < f2){                                                                           //bajando
                 movimientoAba = 1;
+                dis = AjedUtils.distancia(f1, f2);
             }
             if (c2 < c1) {                                                                               //Izquierda
                 movimientoIzq = -1;
+                dis = AjedUtils.distancia(c1, c2);
             }else if(c1 < c2){                                                                           //Derecha
                 movimientoDer = 1;
+                dis = AjedUtils.distancia(c1, c2);
             }
-            for (int i = 1; i < AjedUtils.distancia(f1, f2); i++) {
+            for (int i = 1; i < dis; i++) {
                 if(bien)bien = Tablero.tablero[f1+movimientoArr+movimientoAba][c1+movimientoIzq+movimientoDer].getNombre().equals("       ");
             }
         }
