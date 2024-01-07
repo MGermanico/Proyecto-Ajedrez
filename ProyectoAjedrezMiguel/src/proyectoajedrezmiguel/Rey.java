ae0f4f132a0ed4fe4a4fb4ac1b3c035ca01081db
Miguel <miguelgermanico@gmail.com>
2024-01-07 23:30:57 +0100

    en proceso sistema de jaque en enrroque

--- ProyectoAjedrezMiguel/src/proyectoajedrezmiguel/Rey.java
+++ ProyectoAjedrezMiguel/src/proyectoajedrezmiguel/Rey.java
@@ -26,12 +26,45 @@ public class Rey extends Pieza{
             if(AjedUtils.distancia(f1, f2) < 2 && AjedUtils.distancia(c1, c2) < 2){         //movimiento normal
                 bien = true;
             }else if(f1 == f2 && (c2 == 2 || c2 == 6)){                                     //movimiento de enrroque
-                if (c2 == 2) {
+                boolean blancas = f1 == 1;
+                if (c2 == 2) {                                                                  //enrroque corto
                     Torre temp = (Torre)Tablero.tablero[f1][1];
                     bien = !temp.isSeMovio() && Tablero.tablero[f1][2].getNombre().equals("       ") && Tablero.tablero[f1][3].getNombre().equals("       ");
-                }else if (c2 == 6) {
+                    if(bien)bien = !AjedUtils.JaqueA(blancas);
+                    if(bien){                                                                   //comprobación jaque
+                        Pieza temp2 = Tablero.tablero[f1][3];
+                        Tablero.tablero[f1][3] = Tablero.tablero[f1][4];
+                        Tablero.tablero[f1][4] = temp;
+                        bien = !AjedUtils.JaqueA(blancas);
+                    }
+                    if(bien){                                                                   //comprobación jaque
+                        Pieza temp2 = Tablero.tablero[f1][2];
+                        Tablero.tablero[f1][2] = Tablero.tablero[f1][4];
+                        Tablero.tablero[f1][4] = temp;
+                        bien = !AjedUtils.JaqueA(blancas);
+                    }
+                }else if (c2 == 6) {                                                            //enroque largo
                     Torre temp = (Torre)Tablero.tablero[f1][8];
                     bien = !temp.isSeMovio() && Tablero.tablero[f1][5].getNombre().equals("       ") && Tablero.tablero[f1][6].getNombre().equals("       ") && Tablero.tablero[f1][7].getNombre().equals("       ");
+                    if(bien)bien = !AjedUtils.JaqueA(blancas);
+                    if(bien){                                                                   //comprobación jaque
+                        Pieza temp2 = Tablero.tablero[f1][5];
+                        Tablero.tablero[f1][5] = Tablero.tablero[f1][4];
+                        Tablero.tablero[f1][4] = temp;
+                        bien = !AjedUtils.JaqueA(blancas);
+                    }
+                    if(bien){                                                                   //comprobación jaque
+                        Pieza temp2 = Tablero.tablero[f1][6];
+                        Tablero.tablero[f1][6] = Tablero.tablero[f1][4];
+                        Tablero.tablero[f1][4] = temp;
+                        bien = !AjedUtils.JaqueA(blancas);
+                    }
+                    if(bien){                                                                   //comprobación jaque
+                        Pieza temp2 = Tablero.tablero[f1][7];
+                        Tablero.tablero[f1][7] = Tablero.tablero[f1][4];
+                        Tablero.tablero[f1][4] = temp;
+                        bien = !AjedUtils.JaqueA(blancas);
+                    }
                 }
             }else bien = false;
         }else{                                                                          //no se puede enrroque
